f1bf2af71a72506cebc213a507fb311bc7e588c5
Prince <zizhuang_wang@umail.ucsb.edu>
2018-02-06 13:23:10 -0800

    Update bst.h
    std::string

--- project1/bst.h
+++ project1/bst.h
@@ -2,26 +2,26 @@
 
 #include <string>
 
-template <class T>
+//template <class T>
 struct node{
   int count; // frequency of a word
-  T word; // word
+  std::string word; // word
   node* left; // left child
   node* right; // right child
 
 }
 
-template <class T>
+//template <class T>
 class bst{
 private:
-  node<T> *root; // root of bst
+  node *root; // root of bst
 public:
   void bst();
-  void insert(T word);
-  node* insert(T word, node* t);
-  node* deleteNode(T word);
-  bool search(T word);
+  void insert(std::string word);
+  node* insert(std::string word, node* t);
+  node* deleteNode(std::string word);
+  bool search(std::string word);
   void inorder(node* n);
   void sort();
-  void rangeSearch(T word1, T word2);
+  void rangeSearch(std::string word1, std::string word2);
 }
