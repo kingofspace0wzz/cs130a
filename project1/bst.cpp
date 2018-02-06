0a93f839520150d00ff4d47167f78ef64cbc13bd
kingofspace0wzz <zizhuang_wang@umail.ucsb.edu>
2018-02-06 01:03:04 -0800

    bst

---
+++ project1/bst.cpp
@@ -0,0 +1,12 @@
+#include "bst.h"
+#include <iostream>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+bst::bst(){
+  root = nullptr;
+}
+
+node * bst::insert()
---
+++ project1/bst.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+template <class T>
+struct node{
+  int count; // frequency of a word
+  T word; // word
+  node* left; // left child
+  node* right; // right child
+
+}
+
+template <class T>
+class bst{
+private:
+  node<T> *root; // root of bst
+public:
+  void bst();
+  node* insert(T word);
+  node* deleteNode(T word);
+  bool search(T word);
+  void inorder(node* n);
+  void sort();
+  void rangeSearch(T word1, T word2);
+}
