585b2061a8514d47a548fa3ddc500d89dbe1c80b
kingofspace0wzz <zizhuang_wang@umail.ucsb.edu>
2018-02-09 22:58:41 -0800

    new parser

--- project1/test.cpp
+++ project1/test.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <time.h>
 #include "bst.h"
+#include <sstream>
 
 using namespace std;
 
@@ -12,6 +13,10 @@ bool isLetters(const string & input){
     }
     
     return true;
+}
+
+bool my_predicate(char c){
+    return c == '\'' || !isalpha(c);
 }
 
 int main(){
@@ -25,18 +30,35 @@ int main(){
         cerr << "shit!!!" << endl;
     
     }
-    while(inf){
-        bool alpha = true;
-        std::string strInput;
-        inf >> strInput;
-        if (isLetters(strInput)){
-            if (!isalpha(strInput[strInput.size()-1])){
-                string temp (strInput.begin(), strInput.begin()+strInput.size()-1);
-                strInput = temp;
-            }
-            cout << strInput << endl;   
-            count++;
-        }else
-            continue;
+    string line;
+    while(getline(inf, line)){
+        replace_if(line.begin(), line.end(), my_predicate, ' ');
+        
+        istringstream iss (line);
+        string word;
+        while(iss >> word)
+            cout << word << endl;
     }
+
+
+    // while(inf){
+    //     bool alpha = true;
+    //     string strInput;
+
+    //     inf >> strInput;
+
+
+
+    //     strInput.erase(remove_if(strInput.begin(),strInput.end(),[](char c) {return !isalpha(c);}), strInput.end());
+    //     cout << strInput << endl;
+    //     // if (isLetters(strInput)){
+    //     //     if (!isalpha(strInput[strInput.size()-1])){
+    //     //         string temp (strInput.begin(), strInput.begin()+strInput.size()-1);
+    //     //         strInput = temp;
+    //     //     }
+    //     //     cout << strInput << endl;   
+    //     //     count++;
+    //     // }else
+    //     //     continue;
+    // }
 }
